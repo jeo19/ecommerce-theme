3d66847518a06480bb49f90113f8734f84e5fdf1
jeo19 <terrakkk@outlook.com>
2018-09-10 22:29:36 +0630

    add styledComponents

---
+++ components/theme/signup.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import styled, { injectGlobal, ThemeProvider } from "styled-components";
+const Container = styled.View`
+flex:1  
+background-color: white;
+`;
